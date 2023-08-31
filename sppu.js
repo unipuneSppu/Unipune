76d0bcb9fbf7fa0e24d4e0dd1630a369851934ef
unipuneSppu <143700154+unipuneSppu@users.noreply.github.com>
2023-08-31 14:15:55 +0530

    added sppu.html

---
+++ sppu.js
@@ -0,0 +1,139 @@
+let navBar = document.querySelector("#nav-bar")
+function sidemenu(){
+    navBar.style.width = "250px";
+}
+
+function sidemenu2(){
+    navBar.style.width = "0px"
+}
+
+function testmsg(){
+     document.getElementById("test-msg").innerHTML = "hello world";
+}
+
+//****//
+function robot(){
+    let checkBox = document.getElementById("capCheck");
+    console.log(checkBox.value);
+    if(checkBox.value == "false"){
+        checkBox.value == "true";
+        console.log(checkBox.value)
+    }
+}
+
+
+function showTime(){
+    let seatNumber = document.getElementById("rollNum").value;
+    let momName = document.getElementById("momName").value;
+    let seatList = Object.keys(STUDENTS);
+    //error msg
+    let seatErr = document.getElementById("seat-error");
+    let motherErr = document.getElementById("mother-error"); 
+    let ref = 0;
+    if(seatNumber.length < 10  && momName.length == 0 && seatNumber.length == 0){
+        console.log("working");
+        seatErr.innerText = "Please Enter Seat No.";
+        motherErr.innerText = "Please Enter Mother Name.";
+        if(momName == 0){
+            motherErr.innerText = "Please Enter Mother Name.";
+        }
+        if(seatNumber.length == 0){
+            seatErr.innerText = "Please Enter Seat No.";
+        }
+        if(seatNumber > 0 && seatNumber < 10){
+            seatErr.innerText = "Invalid Seat No.";
+        }
+    
+    }else{
+        if(seatList.includes(seatNumber.trim())){
+            if(momName.toUpperCase() == STUDENTS[seatNumber].motherName){
+                showResult();
+            }else{
+                console.log("mother name not match")
+                motherErr.innerText = "Invalid Mother Name"
+            }
+        }else{
+            console.log(false)
+            seatErr.innerText = "Invalid Seat No."
+        }
+    }
+}
+
+
+//*****/
+
+
+function showResult(){
+    document.getElementById("sppuResult").style.display = "block"
+    document.getElementById("sppuBody").style.display = "none"
+
+
+
+    // inputs
+    let seatNumber = document.getElementById("rollNum").value.toLowerCase();
+    let momName = document.getElementById("momName").value;
+    let seatList = Object.keys(STUDENTS)
+
+    //outputs
+    let seatNoOut = document.getElementById("seatNoOut");
+    let studentname = document.getElementById("studentName");
+    let motherNameOut = document.getElementById("motherNameOut");
+    let prnOut= document.getElementById("prnOut")
+
+ 
+    let ref = 0;
+
+    for(let i = 0;i < seatList.length;i++){
+        if(seatList[i] == seatNumber){
+            if(momName.toUpperCase() == STUDENTS[seatNumber].motherName){
+                seatNoOut.innerText = seatNumber.toUpperCase();
+                studentname.innerText = STUDENTS[seatNumber].name;
+                motherNameOut.innerText = STUDENTS[seatNumber].motherName;
+                prnOut.innerText = STUDENTS[seatNumber].prn
+
+            ref = i;
+            break;
+            }else{
+                console.log("unable to load");
+            }
+        }else{
+            console.log("invalid input")
+        }
+    }
+    var myArray = STUDENTS[seatNumber].subject;
+    console.log(myArray);
+    console.log(myArray[1].subjectName);
+
+    /****/
+    let html = "<table>"
+
+      setTimeout(() => {
+        html += "<thead>";
+        html += "<tr>";
+        html += "<td>" + "Sem" + "</td>";
+        html += "<td>" + "SubCode" + "</td>";
+        html += "<td>" + "SubjectName" + "</td>";
+        html += "<td>" + "Crd" + "</td>";
+        html += "<td>" + "Grd" + "</td>";
+        html += "<td>" + "GP" + "</td>";
+        html += "</tr>";
+        html += "</thead>";
+        for(let i = 0;i < myArray.length;i++){
+          html += "<tr>";
+          html += "<td>" + myArray[i].sem + "</td>";
+          html += "<td>" + myArray[i].subcode + "</td>";
+          html += "<td>" + myArray[i].subjectName + "</td>";
+          html += "<td>" + myArray[i].crd + "</td>";
+          html += "<td>" + myArray[i].grd + "</td>";
+          html += "<td>" + myArray[i].gp + "</td>";
+          html += "</tr>";        
+      }
+      document.getElementById("table").innerHTML = html;
+      },500);
+    /****/   
+}
+
+
+
+
+
